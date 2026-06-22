0ebee3521dee836d9f7dad07bb9d1da2d336a7c9
Whale Joy <afterlifelineonomega@gmail.com>
2026-06-23 02:40:26 +0300

    fix: configure favicon metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,6 +28,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: t('title'),
     description: t('description'),
+    icons: {
+      icon: '/favicon.svg',
+    },
   };
 }
 
